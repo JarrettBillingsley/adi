b51da47208ca91667d6fb3dffa87009c5d0b61f3
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2022-01-16 14:14:36 -0500

    WIP: more instruction printing stuff, this time working on adding styling support in an abstract way.

--- src/arch.rs
+++ src/arch.rs
@@ -140,29 +140,95 @@ impl<'dis, 'img> Iterator for DisasAll<'dis, 'img> {
 }
 
 // ------------------------------------------------------------------------------------------------
+// IPrintStyler, NullPrintStyler
+// ------------------------------------------------------------------------------------------------
+
+/// Trait for applying styling to the output of a [`Printer`]. There are multiple styles which
+/// each have `begin_` and `end_` methods; they all take a [`std::fmt::Write`] which is the
+/// writer associated with a [`PrinterCtx`]. Implementors *may* write things to the writer (e.g.
+/// an HTML styler might write HTML tags to it), or they may ignore it completely and do styling
+/// some other way (e.g. a GUI may create widgets for each style or something).
+pub trait IPrintStyler {
+	/// Begin outputting an instruction mnemonic.
+	fn begin_mnemonic(&mut self, writer: &mut dyn FmtWrite);
+	///
+	fn end_mnemonic(&mut self, writer: &mut dyn FmtWrite);
+
+	/// Begin outputting a register name.
+	fn begin_register(&mut self, writer: &mut dyn FmtWrite);
+	///
+	fn end_register(&mut self, writer: &mut dyn FmtWrite);
+
+	/// Begin outputting a number literal.
+	fn begin_number(&mut self, writer: &mut dyn FmtWrite);
+	///
+	fn end_number(&mut self, writer: &mut dyn FmtWrite);
+
+	/// Begin outputting a meaningful symbol (e.g. braces around address calculations, arithmetic
+	/// operators).
+	fn begin_symbol(&mut self, writer: &mut dyn FmtWrite);
+	///
+	fn end_symbol(&mut self, writer: &mut dyn FmtWrite);
+
+	/// Begin outputting a string or character literal.
+	fn begin_string(&mut self, writer: &mut dyn FmtWrite);
+	///
+	fn end_string(&mut self, writer: &mut dyn FmtWrite);
+
+	/// Begin outputting a comment.
+	fn begin_comment(&mut self, writer: &mut dyn FmtWrite);
+	///
+	fn end_comment(&mut self, writer: &mut dyn FmtWrite);
+}
+
+/// Dummy print styler that ignores all styling commands.
+struct NullPrintStyler;
+
+impl IPrintStyler for NullPrintStyler {
+	fn begin_mnemonic(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn end_mnemonic(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn begin_register(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn end_register(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn begin_number(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn end_number(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn begin_symbol(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn end_symbol(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn begin_string(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn end_string(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn begin_comment(&mut self, _writer: &mut dyn FmtWrite) {}
+	fn end_comment(&mut self, _writer: &mut dyn FmtWrite) {}
+}
+
+// ------------------------------------------------------------------------------------------------
 // PrinterCtx
 // ------------------------------------------------------------------------------------------------
 
 use std::fmt::{ Write as FmtWrite, Error as FmtError, Arguments as FmtArguments };
+pub type FmtResult = Result<(), FmtError>;
 
 use crate::{ Program, IPlatform, Radix, Operand };
 
-pub struct PrinterCtx<'p, 'i, 'w> {
+pub struct PrinterCtx<'p, 'i, 'w, 's> {
 	prog:      &'p Program,
 	inst:      &'i Instruction,
 	mmu_state: MmuState,
 	writer:    &'w mut dyn FmtWrite,
+	styler:    &'s mut dyn IPrintStyler,
 }
 
-impl<'p, 'i, 'w> PrinterCtx<'p, 'i, 'w> {
+impl<'p, 'i, 'w, 's> PrinterCtx<'p, 'i, 'w, 's> {
 	pub fn new(
 		prog: &'p Program,
 		inst: &'i Instruction,
 		mmu_state: MmuState,
-		writer: &'w mut dyn FmtWrite
+		writer: &'w mut dyn FmtWrite,
+		styler: &'s mut dyn IPrintStyler,
 	) -> Self {
-		Self { prog, inst, mmu_state, writer }
+		Self { prog, inst, mmu_state, writer, styler }
 	}
+
+	// --------------------------------------------------------------------------------------------
+	// Misc methods
 
 	pub fn num_ops(&self) -> usize {
 		self.inst.num_ops()
@@ -187,17 +253,65 @@ impl<'p, 'i, 'w> PrinterCtx<'p, 'i, 'w> {
 		self.prog.name_of_va(self.mmu_state, va)
 	}
 
-	pub fn write_str(&mut self, s: &str) -> Result<(), FmtError> {
+	// --------------------------------------------------------------------------------------------
+	// Style methods
+
+	pub fn style_mnemonic(&mut self, f: &dyn Fn(&mut PrinterCtx) -> FmtResult) -> FmtResult {
+		self.styler.begin_mnemonic(self.writer);
+		f(self)?;
+		self.styler.end_mnemonic(self.writer);
+		Ok(())
+	}
+
+	pub fn style_register(&mut self, f: &dyn Fn(&mut PrinterCtx) -> FmtResult) -> FmtResult {
+		self.styler.begin_register(self.writer);
+		f(self)?;
+		self.styler.end_register(self.writer);
+		Ok(())
+	}
+
+	pub fn style_number(&mut self, f: &dyn Fn(&mut PrinterCtx) -> FmtResult) -> FmtResult {
+		self.styler.begin_number(self.writer);
+		f(self)?;
+		self.styler.end_number(self.writer);
+		Ok(())
+	}
+
+	pub fn style_symbol(&mut self, f: &dyn Fn(&mut PrinterCtx) -> FmtResult) -> FmtResult {
+		self.styler.begin_symbol(self.writer);
+		f(self)?;
+		self.styler.end_symbol(self.writer);
+		Ok(())
+	}
+
+	pub fn style_string(&mut self, f: &dyn Fn(&mut PrinterCtx) -> FmtResult) -> FmtResult {
+		self.styler.begin_string(self.writer);
+		f(self)?;
+		self.styler.end_string(self.writer);
+		Ok(())
+	}
+
+	pub fn style_comment(&mut self, f: &dyn Fn(&mut PrinterCtx) -> FmtResult) -> FmtResult {
+		self.styler.begin_comment(self.writer);
+		f(self)?;
+		self.styler.end_comment(self.writer);
+		Ok(())
+	}
+
+	// --------------------------------------------------------------------------------------------
+	// std::fmt::Write methods
+
+	pub fn write_str(&mut self, s: &str) -> FmtResult {
 		self.writer.write_str(s)
 	}
 
-	pub fn write_char(&mut self, c: char) -> Result<(), FmtError> {
+	pub fn write_char(&mut self, c: char) -> FmtResult {
 		self.writer.write_char(c)
 	}
 
 	/// Because of this method, you can use Rust's built-in `write!()` macro on a `PrinterCtx`
 	/// object, like `write!(ctx, "x {}", y)`.
-	pub fn write_fmt(&mut self, args: FmtArguments<'_>) -> Result<(), FmtError> {
+	pub fn write_fmt(&mut self, args: FmtArguments<'_>) -> FmtResult {
 		self.writer.write_fmt(args)
 	}
 }
@@ -224,7 +338,7 @@ pub trait IPrinter {
 	/// This is used to output padding spaces to align operands. Defaults to 8. If set to less
 	/// than the actual longest mnemonic, operands will be misaligned in the output.
 	/// DO NOT IMPLEMENT THIS METHOD IN ANYTHING OTHER THAN CONSTANT TIME.
-	// This is not an associated constant because enum_dispatch doesn't support those.
+	/// (This is not an associated constant because `enum_dispatch` doesn't support those.)
 	fn mnemonic_max_len(&self) -> usize {
 		8
 	}
@@ -237,7 +351,7 @@ pub trait IPrinter {
 
 	/// Prints an indirect memory access where the address is specified by a register.
 	/// It's up to the architecture what this will look like.
-	fn print_indir_reg(&self, ctx: &mut PrinterCtx, reg: u8) -> Result<(), FmtError> {
+	fn print_indir_reg(&self, ctx: &mut PrinterCtx, reg: u8) -> FmtResult {
 		// TODO: these should be implemented by implementors
 		let _ = (ctx, reg);
 		Ok(())
@@ -245,8 +359,7 @@ pub trait IPrinter {
 
 	/// Prints an indirect memory access where the address is specified by a register plus a
 	/// displacement. It's up to the architecture what this will look like.
-	fn print_indir_reg_disp(&self, ctx: &mut PrinterCtx, reg: u8, disp: i64)
-		-> Result<(), FmtError> {
+	fn print_indir_reg_disp(&self, ctx: &mut PrinterCtx, reg: u8, disp: i64) -> FmtResult {
 		// TODO: these should be implemented by implementors
 		let _ = (ctx, reg, disp);
 		Ok(())
@@ -262,7 +375,7 @@ pub trait IPrinter {
 
 	/// Prints the instruction associated with `ctx`. Inserts whitespace padding after
 	/// the mnemonic based on what [`mnemonic_max_len`] returns.
-	fn print_instr(&self, ctx: &mut PrinterCtx) -> Result<(), FmtError> {
+	fn print_instr(&self, ctx: &mut PrinterCtx) -> FmtResult {
 		let width = self.mnemonic_max_len();
 		let mnem = self.get_mnemonic(ctx.get_inst());
 
@@ -273,7 +386,7 @@ pub trait IPrinter {
 	/// Prints all operands of the instruction associated with `ctx`, comma-separated.
 	/// This is only called by [`print_instr`], so if you override that method (maybe
 	/// because your architecture does not fill in all operands), this may go unused.
-	fn print_operands(&self, ctx: &mut PrinterCtx) -> Result<(), FmtError> {
+	fn print_operands(&self, ctx: &mut PrinterCtx) -> FmtResult {
 		for i in 0 .. ctx.num_ops() {
 			if i > 0 {
 				ctx.write_str(", ")?;
@@ -287,7 +400,7 @@ pub trait IPrinter {
 
 	/// Prints the `i`th operand of the instruction associated with `ctx`. This dispatches
 	/// to the appropriate `print_` method based on the operand's type.
-	fn print_operand(&self, ctx: &mut PrinterCtx, i: usize) -> Result<(), FmtError> {
+	fn print_operand(&self, ctx: &mut PrinterCtx, i: usize) -> FmtResult {
 		use crate::Operand::*;
 		use crate::MemIndir::{ self, RegDisp };
 
@@ -308,30 +421,30 @@ pub trait IPrinter {
 	}
 
 	/// Prints the name of a register.
-	fn print_register(&self, ctx: &mut PrinterCtx, r: u8) -> Result<(), FmtError> {
-		ctx.write_str(ctx.register_name(r))
+	fn print_register(&self, ctx: &mut PrinterCtx, r: u8) -> FmtResult {
+		ctx.style_register(&|ctx| ctx.write_str(ctx.register_name(r)))
 	}
 
 	/// Prints an unsigned integer in decimal (base 10).
-	fn print_uint_dec(&self, ctx: &mut PrinterCtx, val: u64) -> Result<(), FmtError> {
-		write!(ctx, "{}", val)
+	fn print_uint_dec(&self, ctx: &mut PrinterCtx, val: u64) -> FmtResult {
+		ctx.style_number(&|ctx| write!(ctx, "{}", val))
 	}
 
 	/// Prints an unsigned integer in hexadecimal (base 16). Defaults to prepending
 	/// the value with `0x` and uses uppercase hex digits.
-	fn print_uint_hex(&self, ctx: &mut PrinterCtx, val: u64) -> Result<(), FmtError> {
-		write!(ctx, "0x{:X}", val)
+	fn print_uint_hex(&self, ctx: &mut PrinterCtx, val: u64) -> FmtResult {
+		ctx.style_number(&|ctx| write!(ctx, "0x{:X}", val))
 	}
 
 	/// Prints an unsigned integer in binary (base 2). Defaults to prepending the
 	/// value with `0b`.
-	fn print_uint_bin(&self, ctx: &mut PrinterCtx, val: u64) -> Result<(), FmtError> {
-		write!(ctx, "0b{:b}", val)
+	fn print_uint_bin(&self, ctx: &mut PrinterCtx, val: u64) -> FmtResult {
+		ctx.style_number(&|ctx| write!(ctx, "0b{:b}", val))
 	}
 
 	/// Prints an unsigned integer with no specified radix. Defaults to using decimal for
 	/// numbers under 16, and hexadecimal otherwise.
-	fn print_uint_no_radix(&self, ctx: &mut PrinterCtx, val: u64) -> Result<(), FmtError> {
+	fn print_uint_no_radix(&self, ctx: &mut PrinterCtx, val: u64) -> FmtResult {
 		if val < 0x10 {
 			self.print_uint_dec(ctx, val)
 		} else {
@@ -340,29 +453,35 @@ pub trait IPrinter {
 	}
 
 	/// Prints a signed integer in decimal (base 10).
-	fn print_int_dec(&self, ctx: &mut PrinterCtx, val: i64) -> Result<(), FmtError> {
-		write!(ctx, "{}", val)
+	fn print_int_dec(&self, ctx: &mut PrinterCtx, val: i64) -> FmtResult {
+		ctx.style_number(&|ctx| write!(ctx, "{}", val))
 	}
 
 	/// Prints a signed integer in hexadecimal (base 16). Defaults to prepending
 	/// the value with `0x` and uses uppercase hex digits. Negative numbers are displayed
 	/// like `-0x12` for `-18`, rather than in 2's complement.
-	fn print_int_hex(&self, ctx: &mut PrinterCtx, val: i64) -> Result<(), FmtError> {
-		let sign = if val < 0 { "-" } else { "" };
-		write!(ctx, "{}0x{:X}", sign, val.abs())
+	fn print_int_hex(&self, ctx: &mut PrinterCtx, val: i64) -> FmtResult {
+		if val < 0 {
+			ctx.style_symbol(&|ctx| ctx.write_char('-'))?;
+		}
+
+		ctx.style_number(&|ctx| write!(ctx, "0x{:X}", val.abs()))
 	}
 
 	/// Prints a signed integer in binary (base 2). Defaults to prepending the
 	/// value with `0b`. Negative numbers are displayed like `-0b101` for `-5`, rather than
 	/// in 2's complement.
-	fn print_int_bin(&self, ctx: &mut PrinterCtx, val: i64) -> Result<(), FmtError> {
-		let sign = if val < 0 { "-" } else { "" };
-		write!(ctx, "{}0b{:b}", sign, val.abs())
+	fn print_int_bin(&self, ctx: &mut PrinterCtx, val: i64) -> FmtResult {
+		if val < 0 {
+			ctx.style_symbol(&|ctx| ctx.write_char('-'))?;
+		}
+
+		ctx.style_number(&|ctx| write!(ctx, "0b{:b}", val.abs()))
 	}
 
 	/// Prints a signed integer with no specified radix. Defaults to using decimal for
 	/// numbers under 16, and hexadecimal otherwise.
-	fn print_int_no_radix(&self, ctx: &mut PrinterCtx, val: i64) -> Result<(), FmtError> {
+	fn print_int_no_radix(&self, ctx: &mut PrinterCtx, val: i64) -> FmtResult {
 		if val.abs() < 0x10 {
 			self.print_int_dec(ctx, val)
 		} else {
@@ -372,7 +491,7 @@ pub trait IPrinter {
 
 	/// Prints the name of thing to which a virtual address refers, based on the MMU state
 	/// associated with `ctx`.
-	fn print_va(&self, ctx: &mut PrinterCtx, va: VA) -> Result<(), FmtError> {
+	fn print_va(&self, ctx: &mut PrinterCtx, va: VA) -> FmtResult {
 		ctx.write_str(&ctx.name_of_va(va))
 	}
 }
