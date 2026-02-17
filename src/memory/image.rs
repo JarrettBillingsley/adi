d0a95d76763b4d53f92d55ade6d0978cd0ce09d8
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2026-02-17 13:06:42 -0500

    `Image::new_from_file` is now more flexible in the type of path it accepts

--- src/memory/image.rs
+++ src/memory/image.rs
@@ -166,9 +166,9 @@ impl Image {
 	}
 
 	/// Convenience ctor to load the data directly from a file.
-	pub fn new_from_file(filename: &str) -> std::io::Result<Self> {
-		let data = std::fs::read(filename)?;
-		Ok(Self::new(filename, &data))
+	pub fn new_from_file<P: AsRef<std::path::Path>>(filename: P) -> std::io::Result<Self> {
+		let data = std::fs::read(&filename)?;
+		Ok(Self::new(&filename.as_ref().to_string_lossy().into_owned(), &data))
 	}
 
 	/// Create a *new* image whose data is a copy of a range of this one's.
