020c8e1492fa1f9e4efc39313f223580beade6af
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2026-04-01 16:35:24 -0400

    added RefMap::outrefs_in_range

--- src/program/refmap.rs
+++ src/program/refmap.rs
@@ -5,6 +5,7 @@ use std::collections::{
 
 	btree_map::Iter as BTreeIter,
 };
+use std::ops::{ RangeBounds, Bound };
 
 use crate::memory::EA;
 
@@ -76,6 +77,30 @@ impl RefMap {
 	pub fn get_outrefs(&self, src: EA) -> Option<&RefSet> {
 		assert!(src.is_resolved());
 		self.outrefs.get(&src)
+	}
+
+	/// Get all outrefs from a range of EAs, in order of referrers. Only ranges of the form `a .. b`
+	/// (inclusive start, exclusive end) are allowed. The end EA is never accessed, only used as a
+	/// bound, so it's okay if it's the first byte past the end of a segment.
+	///
+	/// Panics if `range` is not a valid kind of range, or if either end is unresolved.
+	pub fn outrefs_in_range(&self, range: impl RangeBounds<EA>) -> impl Iterator<Item = (EA, EA)> {
+		let start = match range.start_bound() {
+			Bound::Included(&s) => s,
+			_                   => panic!("only ranges of the form `a .. b` are allowed"),
+		};
+
+		let end = match range.end_bound() {
+			Bound::Excluded(&e) => e,
+			_                   => panic!("only ranges of the form `a .. b` are allowed"),
+		};
+
+		assert!(start.is_resolved(), "start unresolved");
+		assert!(end.is_resolved(),   "end unresolved");
+
+		self.outrefs.range(start .. end).map(|(src, dsts): (&EA, &RefSet)| {
+			dsts.iter().map(|dst| (*src, *dst))
+		}).flatten()
 	}
 
 	/// Iterator over all outrefs in the entire map.
@@ -166,5 +191,41 @@ mod tests {
 		assert_eq!(ref_map.get_inrefs(e), None);
 		assert_eq!(ref_map.get_outrefs(e), None);
 		assert_eq!(ref_map.all_outrefs().count(), 0);
+	}
+
+	#[test]
+	#[allow(unused)]
+	fn outrefs_in_range() {
+		let mut ref_map = RefMap::new();
+		fn ea(offs: u64) -> EA { EA::new(SegId(0), offs) }
+
+		let f1_a   = EA::new(SegId(0), 0x00);
+		let f1_b   = EA::new(SegId(0), 0x01);
+		let f1_c   = EA::new(SegId(0), 0x02);
+		let f1_d   = EA::new(SegId(0), 0x03);
+		let f1_e   = EA::new(SegId(0), 0x04);
+		let f1_end = EA::new(SegId(0), 0x05);
+
+		let f2_a   = EA::new(SegId(0), 0x10);
+		let f2_b   = EA::new(SegId(0), 0x11);
+		let f2_c   = EA::new(SegId(0), 0x12);
+		let f2_d   = EA::new(SegId(0), 0x13);
+		let f2_e   = EA::new(SegId(0), 0x14);
+		let f2_end = EA::new(SegId(0), 0x15);
+
+		ref_map.add(f1_a, f1_c);
+		ref_map.add(f1_b, f2_a);
+		ref_map.add(f1_d, f1_a);
+		ref_map.add(f2_a, f2_b);
+		ref_map.add(f2_a, f2_c);
+		ref_map.add(f2_e, f2_d);
+
+		assert_eq!(
+			ref_map.outrefs_in_range(f1_a .. f1_end).collect::<Vec<_>>(),
+			&[(f1_a, f1_c), (f1_b, f2_a), (f1_d, f1_a)]);
+
+		assert_eq!(
+			ref_map.outrefs_in_range(f2_a .. f2_end).collect::<Vec<_>>(),
+			&[(f2_a, f2_b), (f2_a, f2_c), (f2_e, f2_d)]);
 	}
 }
