14fa2097d0bbc25704f7c9ea06684e61d4240999
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2026-04-01 21:38:51 -0400

    reg usage wip

--- src/program/analysis/pass_regusage.rs
+++ src/program/analysis/pass_regusage.rs
@@ -1,5 +1,6 @@
 
 use crate::program::{ Program };
+use crate::program::analysis::callgraph::*;
 
 // ------------------------------------------------------------------------------------------------
 // Whole-program register usage
@@ -7,7 +8,11 @@ use crate::program::{ Program };
 
 impl Program {
 	pub(super) fn reg_usage_pass(&mut self) {
-		log::info!("register usage");
+		log::info!("------------------------------------------------------------------------");
+		log::info!("- begin register usage pass");
+
+
+
 		// TODO: if a function's reg usage has been analyzed before, its *arguments and clobbers*
 		// cannot have changed, but its *return* regs may have.
 
